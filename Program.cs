9cd7181904a0e655baec30221882234fc2197480
Aykhan-Ahmadzada <100864630+Drongo-J@users.noreply.github.com>
2023-01-11 17:25:59 +0400

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -6,10 +6,42 @@ using System.Threading.Tasks;
 
 namespace AutoResetEvent
 {
-    internal class Program
+    public class Program
     {
+        private static AutoResetEvent _workerEvent = new AutoResetEvent(false);
+
         static void Main(string[] args)
         {
+            Thread thread = new Thread(() => Proc(10));
+            thread.Start();
+            Console.WriteLine("Waiting for Proc function");
+            _workerEvent.WaitOne();
+            Console.WriteLine("Starting some process");
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(i);
+                Thread.Sleep(1000);
+            }
+            _workerEvent.Set();
+            Console.WriteLine("Worker is doing some job , Let's wait !");
+            _workerEvent.WaitOne();
+            Console.WriteLine("Completed");
+
+        }
+
+        private static void Proc(int v)
+        {
+            Console.WriteLine("Starting Some Function");
+            Thread.Sleep(1000);
+            Console.WriteLine("Okay");
+            _workerEvent.Set();
+            Console.WriteLine("Main Thread is working .I am waiting for it");
+            _workerEvent.WaitOne();
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine($"Proc : {i}");
+                Thread.Sleep(1000);
+            }
         }
     }
 }
